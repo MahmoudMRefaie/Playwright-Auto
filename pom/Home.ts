3be695ccf66c156f36fe6189cc31428c1183bd32
Mahmoud Refaie <mahmoud.refaie22@gmail.com>
2024-06-02 11:43:50 +0300

    Edit userHeader at home page to be suitable for mobile chrome

--- pom/Home.ts
+++ pom/Home.ts
@@ -3,13 +3,15 @@ import { Locator, Page } from "@playwright/test";
 export class Home {
     readonly page: Page;
     readonly baseURL: string;
-    readonly user: Locator;
+    readonly userHeader: Locator;
+    readonly user_name: Locator;
     readonly userDropdown: Locator;
 
     constructor(page: Page, baseURL?: string) {
         this.page = page;
         this.baseURL = baseURL ?? '/';
-        this.user = page.locator("[class='oxd-userdropdown-name']");
+        this.userHeader = page.locator("[class='oxd-userdropdown']")
+        this.user_name = page.locator("[class='oxd-userdropdown-name']");
         this.userDropdown = page.locator("[class='oxd-dropdown-menu']");
     }
 
@@ -19,7 +21,7 @@ export class Home {
 
     async logout() {
         console.log('Logout');
-        await this.user.click();
+        await this.userHeader.click();
         await this.userDropdown.getByText('Logout').click();
     }
 }
