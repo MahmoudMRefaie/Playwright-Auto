db659f43a59b7f21d21a1c8579efc59fa4202ab7
Mahmoud Refaie <mahmoud.refaie22@gmail.com>
2024-06-04 14:31:23 +0300

    attach file test

--- pom/UserProfile.ts
+++ pom/UserProfile.ts
@@ -4,16 +4,45 @@ export class UserProfile {
     readonly page: Page;
     readonly baseURL: string;
     readonly user_name: Locator;
+    readonly personalDetailsAddAttachement: Locator;
+    readonly browseBtn: Locator;
+    readonly uploadedFileName: Locator;
+    readonly saveAttachement: Locator;
 
     constructor(page: Page, baseURL?: string) {
         this.page = page;
         this.baseURL = baseURL ?? '/';
         this.user_name = page.locator("[class='orangehrm-edit-employee-name'] h6");
+        this.personalDetailsAddAttachement = page.locator("[class='oxd-button oxd-button--medium oxd-button--text']")
+        this.browseBtn = page.locator("[class='oxd-file-button']")
+        this.uploadedFileName = page.locator('.oxd-file-input-div')
+        this.saveAttachement = page.locator('.orangehrm-attachment [type="submit"]')
 
     }
 
     async goto(userId: string) {
         await this.page.goto(this.baseURL + `/web/index.php/pim/viewMemberships/empNumber/${userId}`)
+    }
+
+    async addAttachement(attachementFile: string){
+        console.log(`Attach file: ${attachementFile}`)
+        await this.personalDetailsAddAttachement.click();
+
+        let [fileChooser] = await Promise.all([         // Using fileChooser due to attachement at dymanic element not input.
+            // It is important to call waitForEvent before click to set up waiting.
+            this.page.waitForEvent('filechooser'),
+            // Opens the file chooser.
+            this.browseBtn.click()
+          ]);
+    
+          await fileChooser.setFiles(attachementFile);
+    }
+
+    async addAttachementAndSave(attachementFile: string){
+
+        await this.addAttachement(attachementFile);
+        
+        await this.saveAttachement.click();
     }
 
 
--- tests/user-profile.spec.ts
+++ tests/user-profile.spec.ts
@@ -3,27 +3,30 @@ import playwrightConfig from '../playwright.config';
 import { Login } from '../pom/Login';
 import { Home } from '../pom/Home';
 import { UserProfile } from '../pom/UserProfile';
+const path = require('path');
 
-test.describe.configure({ retries: 2 });
 test.describe('UserProfile', async() => {
 
     let page: Page;
     let login: Login;
     let home: Home;
     let userProfile: UserProfile;
+    var homeUserName
 
     test('Verify on the user name', async() => {
-
-        var homeUserName = await home.getUserName();
-        await home.openMyInfo();
 
         await expect(userProfile.user_name).toHaveText(homeUserName);
     });
 
-    // test('Verify the user can logout successfully', async() => {
+    test('Add attachement to personal details', async() => {
+
+        // Error: fileChooser.setFiles: ENOENT: no such file or directory, access 'D:\MR\Learn\Projects\Playwright-Auto\resources\images\test_image.png'
+        // [SOLVED]: Right click on file and copy relative path
+        await userProfile.addAttachementAndSave('resources\\Images\\test_images.png');
+        await expect(userProfile.uploadedFileName).toHaveText('test_images.png');
         
-    //     await home.logout();
-    // });
+        page.waitForEvent('close');
+    });
 
     test.beforeAll('BeforeAll', async({ browser }, testInfo) => {
         page = await browser.newPage();     //Reuse single page between tests  ->  https://playwright.dev/docs/test-retries#reuse-single-page-between-tests
@@ -35,6 +38,9 @@ test.describe('UserProfile', async() => {
 
         login.goto();
         await login.login('Admin', 'admin123');
+
+        homeUserName = await home.getUserName();
+        await home.openMyInfo();
     });
 
     test.afterAll('AfterAll', async() => {
